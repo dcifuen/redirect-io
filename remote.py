ee1f7d1b84927d69ac4452095752f5321f0ea2f2
dcifuen <dcifuen@gmail.com>
2015-11-10 15:45:17 -0500

    Adding libraries

--- remote.py
+++ remote.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from getpass import getpass
 import logging
+from getpass import getpass
 
 from google.appengine.ext.remote_api import remote_api_stub
 
@@ -19,10 +19,12 @@ def add_redirect(source, destination, permanent=True):
 def auth_func():
     return raw_input('Username:'), getpass('Password:')
 
+
 remote_api_stub.ConfigureRemoteApi(
     None, '/_ah/remote_api', auth_func,
-    #'localhost:8080',
+    # 'localhost:8080',
     'redirect-io.appspot.com'
 )
 
-add_redirect('redirect-io.appspot.com', 'https://github.com/dcifuen/redirect-io', True)
+add_redirect('redirect-io.appspot.com',
+             'https://github.com/dcifuen/redirect-io', True)
